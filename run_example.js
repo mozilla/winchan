d8a60c53c5e4f728a3a952c6003a8ab9b5e84bea
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-12-12 10:19:35 -0700

    run_example uses postprocess to make it easy to target external IPs for local testing

--- run_example.js
+++ run_example.js
@@ -2,17 +2,27 @@
 
 const
 express = require('express'),
-path = require('path');
+path = require('path'),
+postprocess = require('postprocess');
+
+const IP_ADDRESS = process.env['IP_ADDRESS'] || '127.0.0.1';
 
 var client = express.createServer(),
     server = express.createServer();
 
+var subMiddleware = postprocess(function(req, buf) {
+  var re = new Regexp('127\.0\.0\.1', 'g');
+  return buf.replace(re, IP_ADDRESS);
+});
+
 client
   .use(express.static(path.join(__dirname)))
+  .use(subMiddleware)
   .listen(8100);
 
 server
   .use(express.static(path.join(__dirname)))
+  .use(subMiddleware)
   .listen(8200);
 
-console.log("open http://127.0.0.1:8100/example/parent.html");
+console.log("open http://" + IP_ADDRESS + ":8100/example/parent.html");
