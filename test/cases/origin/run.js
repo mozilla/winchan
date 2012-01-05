a04e7e10f0d689a8e7907b8d43d3744c8bab64dc
Shane Tomlinson <stomlinson@mozilla.com>
2012-01-05 11:20:01 +0000

    Enabling tests to be run on hosts other than localhost.

--- test/cases/origin/run.js
+++ test/cases/origin/run.js
@@ -15,10 +15,12 @@ $(document).ready(function(){
   asyncTest("different origin correctly reported", function() {
     var argString = "This is a string we'll send into and back from the dialog: " +
       (new Date()).toString();
-    var other_url = 'http://127.0.0.1:8200/test/cases/origin/child.html';
+    var other_host = document.location.protocol + "//" + document.location.hostname + ":8200";
+
+    var other_url = other_host + '/test/cases/origin/child.html';
     WinChan.open({
       url: other_url,
-      relay_url: "http://127.0.0.1:8200/relay.html",
+      relay_url: other_host + "/relay.html",
       window_features: "width=700,height=375"
     }, function(err, resp) {
       equal(resp, /^(https?:\/\/[-_a-zA-Z\.0-9:]+)/.exec(window.location.href)[1]);
