fa4fe429b57a92b91b9906cbd6581ead37d2fac4
Sean McArthur <sean.monstar@gmail.com>
2012-07-05 18:23:10 -0700

    adding comment about being specific to Opera 12

--- winchan.js
+++ winchan.js
@@ -134,6 +134,8 @@
             try {
               w.close();
             } catch (securityViolation) {
+              // This happens in Opera 12 sometimes
+              // see https://github.com/mozilla/browserid/issues/1844
               messageTarget.postMessage(CLOSE_CMD, origin);
             }
           }
